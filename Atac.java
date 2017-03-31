17fb0b2bfa4de86c1ed6477ece6eaa0492826183
Tina <tchen9@stuy.edu>
2017-03-31 11:45:33 -0400

    interface

---
+++ Atac.java
@@ -0,0 +1,15 @@
+//Tina Chen, Caleb Smith-Salzberg, Angel Ng
+//Team CATScan
+//APCS2 pd5
+//Lab02 -- All Hands on Deque! (Not Schenectady; rather, synecdoche.)
+//2017-03-31
+
+public class Atac{
+    DLLNode _head;
+    DLLNode _tail;
+    int size;
+    
+    public Atac{
+	_head = _tail = null;
+    }
+}
---
+++ Deque.java
@@ -0,0 +1,14 @@
+//Tina Chen, Caleb Smith-Salzberg, Angel Ng
+//Team CATScan
+//APCS2 pd5
+//Lab02 -- All Hands on Deque! (Not Schenectady; rather, synecdoche.)
+//2017-03-31
+
+public interface Deque<T>{
+    //public abstract void addFirst( T e);
+    //public abstract void addLast( T e);
+    //public abstract T peekFirst();
+    //public abstract T peekLast();
+    //public abstract T removeFirst();
+    //public abstract T removeLast();
+}
