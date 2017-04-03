4b127fdaed40880735e85273c33845907e5c43c1
Angel Ng <ang7@stuy.edu>
2017-04-03 11:25:09 -0400

    remove methods

--- Atac.java
+++ Atac.java
@@ -10,53 +10,74 @@ public class Atac<T> implements Deque<T>{
     private int size;
     
     public Atac(){
-	_head = new DLLNode (null, null, null);
-	_tail = _head;
+	_head = _tail = null; 
 	size = 0;
     }
+
     public void addFirst( T e ){
 	DLLNode<T> toAdd = new DLLNode( e, null, null );
-	if ( _head.getCargo()==null )
+	if ( _head==null )
 	    _tail = toAdd;
-    else {
+	else {
 	    toAdd.setNext( _head );
 	    _head.setPrev( toAdd );
 	}
 	_head = toAdd;
 	size += 1;
     }
+
     public void addLast( T e){
 	DLLNode<T> toAdd = new DLLNode( e, null, null );
-	if ( _head.getCargo()==null )
+	if ( _head==null )
 	    _head = toAdd;
-    else {
-	_tail.setNext( toAdd);
-	toAdd.setPrev(_tail);
-    }
+	else {
+	    _tail.setNext( toAdd);
+	    toAdd.setPrev(_tail);
+	}
 	_tail = toAdd;
 	size += 1;
     }
+
     public T peekFirst(){
 	return _head.getCargo();
     }
+
     public T peekLast(){
 	return _tail.getCargo();
     }
+
     public String toString(){
 	String ret = "";
-	DLLNode<T> temp = new DLLNode(null, null,null);
-	temp = _head;
+	DLLNode<T> temp = new DLLNode<T>(null, null, null); 
+	temp = _head; 
 	while (temp != null){
-	    ret += temp.getCargo();
+	    ret += temp.getCargo() + " ";
 	    temp = temp.getNext();
 	}
 	return ret;
     }
+
+    public T removeFirst(){
+	DLLNode<T> first = _head; 
+	_head = _head.getNext();
+	_head.setPrev(null); 
+	return first.getCargo(); 
+    }
+
+    public T removeLast(){
+	DLLNode<T> last = _tail; 
+	_tail = _tail.getPrev(); 
+	_tail.setNext(null); 
+	return last.getCargo();
+    }
+
     public static void main(String[] args){
 	Atac<String> bob = new Atac<String>();
 	bob.addLast("a");
 	bob.addLast("b");
 	bob.addFirst("z");
+	System.out.println(bob);
+	bob.removeLast();
 	System.out.println(bob);
     }
 }
